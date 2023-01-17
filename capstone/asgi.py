99025fb6cdb7d02896b0b019f9dcf8f991cc5bc8
Mateusz Urban <58361072+cyrisek@users.noreply.github.com>
2023-01-17 16:46:50 +0000

    changing asgi file

--- capstone/asgi.py
+++ capstone/asgi.py
@@ -1,12 +1,13 @@
-import chat.routing
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
+from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+
+
+import chat.routing
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
