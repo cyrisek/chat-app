5e9129ec75e0b8000932943e3d4ac07026c392c0
Mateusz Urban <58361072+cyrisek@users.noreply.github.com>
2023-01-17 16:56:36 +0000

    changing settings

--- capstone/settings.py
+++ capstone/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    "daphne",
     'chat',
+    "daphne",
     'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
