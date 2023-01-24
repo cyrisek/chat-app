1b4a2053f0857471b9993edf81ae8feb1413157f
Mateusz Urban <58361072+cyrisek@users.noreply.github.com>
2023-01-24 16:40:59 +0000

    migrate to postgreSQL

--- capstone/settings.py
+++ capstone/settings.py
@@ -89,12 +89,13 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        "TEST": {
-            "NAME": BASE_DIR / "db.sqlite3",
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CaWk6yVl1GcfZvL16Vvx',
+        'HOST': 'containers-us-west-180.railway.app',
+        'PORT': '7490',
     }
 }
 
